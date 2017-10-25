cc174f4eaf514e57c98aa6e2522abea80b59adf0
Ethan Turner <ezturner@washington.edu>
2017-10-25 16:07:34 -0700

    Creating project structure

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+README = """
+See the README on `GitHub
+<https://github.com/uw-it-aca/uw_aws_message>`_.
+"""
+
+# The VERSION file is created by travis-ci, based on the tag name
+version_path = 'uw_aws_message/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+url = "https://github.com/uw-it-aca/uw-aws-message"
+setup(
+    name='UW-RestClients-Core',
+    version=VERSION,
+    packages=['uw_aws_message'],
+    author="UW-IT AXDD",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=[],
+    license='Apache License, Version 2.0',
+    description=('A client for processing AWS SNS and SQS messages from '
+                 'University of Washington data sources.'),
+    long_description=README,
+    url=url,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
